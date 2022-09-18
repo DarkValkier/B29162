742305ddb109a8521e926697fcde0295103212c3
DarkValkier <darkvalkier@gmail.com>
2022-09-18 13:09:04 +0300

    Добавлена функция sum

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
+def sum(a, b):
+    return a + b
+
+
 print('Калькулятор')
 a = int(input('Введите первое число:'))
 b = int(input('Введите второе число:'))
-print(f'{a} + {b} = {a + b}')
+print(f'{a} + {b} = {sum(a, b)}')
 print(f'{a} - {b} = {a - b}')
 print(f'{a} * {b} = {a * b}')
 print(f'{a} / {b} = {a / b}')
