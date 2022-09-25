815ab98e6b670a4b36a3a0d6fd59e9778215daee
DarkValkier <darkvalkier@gmail.com>
2022-09-25 12:36:42 +0300

    В качестве второго игрока используется Samurai

--- Lesson_2/main.py
+++ Lesson_2/main.py
@@ -1,9 +1,10 @@
 from Lesson_1.character import Character
 from berserk import Berserk
 from paladin import Paladin
+from samurai import Samurai
 
 p1 = Berserk(name='Berserk', hp=100, damage=8, armor=0)
-p2 = Paladin(name='Paladin', hp=100, damage=10, armor=0)
+p2 = Samurai(name='Samurai', hp=100, damage=10, armor=0)
 
 while p1.hp > 0 and p2.hp > 0:
     print(p1)
