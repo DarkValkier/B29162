ad30d532ea0221ab5bff37a1d71686916cc14fa6
DarkValkier <darkvalkier@gmail.com>
2022-10-02 14:07:06 +0300

    Сменено название armor на defence

--- Lesson_1/main.py
+++ Lesson_1/main.py
@@ -1,7 +1,7 @@
 from character import Character
 
-player1 = Character(name='Vasya', hp=100, damage=10, armor=0)
-player2 = Character(name='Petya', hp=90, damage=12, armor=0)
+player1 = Character(name='Vasya', hp=100, damage=10, defence=0)
+player2 = Character(name='Petya', hp=90, damage=12, defence=0)
 print(player1)
 print(player2)
 
