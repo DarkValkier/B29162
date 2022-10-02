50ba81b6671ccda577d7b13f01dfa935430273cd
DarkValkier <darkvalkier@gmail.com>
2022-10-02 14:08:15 +0300

    Создан класс Item с функционалом предметов

---
+++ Lesson_4/item.py
@@ -0,0 +1,14 @@
+class Item:
+    def __init__(self, name, stats=None,
+                 durability=1, unbreakable=False):
+        self.name = name
+        self.stats = stats
+        self.durability = durability
+        self.unbreakable = unbreakable
+
+    def use(self):
+        if not self.unbreakable:
+            if self.durability <= 0:
+                return None
+            self.durability -= 1
+        return self.stats
